ed852188a5b5c66224f450346864670f4c16c952
Eugene-Usachev <87601294+Eugene-Usachev@users.noreply.github.com>
2023-09-10 11:59:28 +0300

    Made it fast

--- internal/client/client.go
+++ internal/client/client.go
@@ -71,38 +71,254 @@
 package client
 
 import (
+	"fmt"
+	"github.com/Eugene-Usachev/fastbytes"
+	"github.com/Eugene-Usachev/go-connector/internal/constants"
 	"github.com/Eugene-Usachev/go-connector/internal/pipe"
+	"github.com/Eugene-Usachev/go-connector/internal/result"
 	"log"
+	"net"
+	"sync"
+	"time"
 )
 
 type Client struct {
 	pool chan *pipe.Pipe
 }
 
-func NewClient(host, port string, par int) *Client {
+type Config struct {
+	Host               string
+	Port               string
+	Par                int
+	MaxQueueSize       int
+	MaxWriteBufferSize int
+}
+
+func NewClient(cfg *Config) *Client {
 	c := &Client{
-		pool: make(chan *pipe.Pipe, par),
+		pool: make(chan *pipe.Pipe, cfg.Par),
 	}
 
-	for i := 0; i < par; i++ {
-		pipeImpl, err := pipe.NewPipe(host, port)
-		if err != nil {
-			log.Println("[NimbleDB] Error creating pipe: ", err)
-		}
+	connPool := &sync.Pool{
+		New: func() interface{} {
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", cfg.Host, cfg.Port))
+			if err != nil {
+				log.Println("[NimbleDB] Error connecting to the database: ", err)
+				return nil
+			}
+			return conn
+		},
+	}
+
+	pipeCfg := &pipe.Config{
+		ConnPool:           connPool,
+		MaxQueueSize:       cfg.MaxQueueSize,
+		MaxWriteBufferSize: cfg.MaxWriteBufferSize,
+	}
+
+	for i := 0; i < cfg.Par; i++ {
+		pipeImpl := pipe.NewPipe(pipeCfg)
 		go pipeImpl.Start()
+		time.Sleep(17 * time.Microsecond)
+		go pipeImpl.StartTimer()
+
 		c.pool <- pipeImpl
 	}
 
 	return c
 }
 
+func (c *Client) CallFunc(f func(conn *pipe.Pipe) chan pipe.Res) ([]byte, error) {
+	conn := <-c.pool
+	ch := f(conn)
+	c.pool <- conn
+	var res pipe.Res
+	select {
+	case res = <-ch:
+	case <-time.After(3 * time.Second):
+		return nil, fmt.Errorf("timeout")
+	}
+	if res.Err != nil {
+		return nil, res.Err
+	}
+	if len(res.Slice) < 1 {
+		return nil, fmt.Errorf("empty response")
+	}
+	err := result.DefineError(res.Slice[0])
+	if err != nil {
+		return nil, err
+	}
+	return res.Slice[:], nil
+}
+
 // Ping returns true if the connection is alive and false otherwise.
 func (c *Client) Ping() bool {
-	conn := <-c.pool
-	ch := conn.Ping()
-	c.pool <- conn
-	res := <-ch
-	return res.Err == nil
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.Ping()
+	})
+
+	if err != nil || res[0] != constants.Ping {
+		return false
+	}
+	return true
+
+	// TODO r
+	//conn := <-c.pool
+	//ch := conn.Ping()
+	//c.pool <- conn
+	//res := <-ch
+	//return res.Err == nil
+}
+
+func (c *Client) CreateSpace(engineType constants.SpaceEngineType, size uint16, name []byte) result.Result[uint16] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.CreateSpace(engineType, size, name)
+	})
+	if err != nil {
+		return result.Result[uint16]{
+			Value: 0,
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+	return result.Result[uint16]{
+		Value: fastbytes.B2U16(res[1:3]),
+		Err:   nil,
+		IsOk:  true,
+	}
+	// TODO r
+	//conn := <-c.pool
+	//ch := conn.CreateSpace(engineType, size, name)
+	//c.pool <- conn
+	//res := <-ch
+	//if res.Err != nil {
+	//	return result.Result[uint16]{
+	//		Value: 0,
+	//		Err:   res.Err,
+	//		IsOk:  false,
+	//	}
+	//}
+	//err := result.DefineError(res.Slice[0])
+	//if err != nil {
+	//	return result.Result[uint16]{
+	//		Value: 0,
+	//		Err:   err,
+	//		IsOk:  false,
+	//	}
+	//}
+	//return result.Result[uint16] {
+	//	Value: fastbytes.B2U16(res.Slice[1:3]),
+	//	Err:   nil,
+	//	IsOk:  true,
+	//}
+}
+
+func (c *Client) GetSpacesNames() result.Result[[]string] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.GetSpacesNames()
+	})
+
+	if err != nil {
+		return result.Result[[]string]{
+			Value: nil,
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+
+	offset, length := 0, len(res)
+	arr := []string{}
+
+	for {
+		if offset == length {
+			break
+		}
+		size := int(fastbytes.B2U16(res[offset : offset+2]))
+		arr = append(arr, string(res[offset+2:offset+2+size]))
+		offset += 2 + size
+	}
+
+	return result.Result[[]string]{
+		Value: arr,
+		Err:   nil,
+		IsOk:  true,
+	}
+}
+
+func (c *Client) Insert(key []byte, value []byte, spaceId uint16) result.Result[result.Void] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.Insert(key, value, spaceId)
+	})
+	if err != nil || res[0] != constants.Done {
+		return result.Result[result.Void]{
+			Value: result.Void{},
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+
+	return result.Result[result.Void]{
+		Value: result.Void{},
+		Err:   nil,
+		IsOk:  true,
+	}
+}
+
+func (c *Client) Set(key []byte, value []byte, spaceId uint16) result.Result[result.Void] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.Set(key, value, spaceId)
+	})
+	if err != nil || res[0] != constants.Done {
+		return result.Result[result.Void]{
+			Value: result.Void{},
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+
+	return result.Result[result.Void]{
+		Value: result.Void{},
+		Err:   nil,
+		IsOk:  true,
+	}
+}
+
+func (c *Client) Get(key []byte, spaceId uint16) result.Result[[]byte] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.Get(key, spaceId)
+	})
+	if err != nil || res[0] != constants.Done {
+		return result.Result[[]byte]{
+			Value: nil,
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+
+	return result.Result[[]byte]{
+		Value: res[2:],
+		Err:   nil,
+		IsOk:  true,
+	}
+}
+
+func (c *Client) Delete(key []byte, spaceId uint16) result.Result[result.Void] {
+	res, err := c.CallFunc(func(conn *pipe.Pipe) chan pipe.Res {
+		return conn.Delete(key, spaceId)
+	})
+	if err != nil || res[0] != constants.Done {
+		return result.Result[result.Void]{
+			Value: result.Void{},
+			Err:   err,
+			IsOk:  false,
+		}
+	}
+
+	return result.Result[result.Void]{
+		Value: result.Void{},
+		Err:   nil,
+		IsOk:  true,
+	}
 }
 
 //// CreateSpace creates a new space in the database. CreateSpace returns the ID of the space.
--- internal/pipe/pipe.go
+++ internal/pipe/pipe.go
@@ -1,7 +1,6 @@
 package pipe
 
 import (
-	"fmt"
 	"github.com/Eugene-Usachev/fastbytes"
 	"github.com/Eugene-Usachev/go-connector/internal/constants"
 	"net"
@@ -11,7 +10,6 @@ import (
 
 const (
 	bufferSize = 64 * 1024
-	volume     = 1024
 )
 
 type Slice []byte
@@ -27,7 +25,7 @@ type Res struct {
 
 type Pipe struct {
 	writeBuf []byte
-	conn     net.Conn
+	connPool *sync.Pool
 
 	readBuf []byte
 	// responseBuf is a buffer for storing a big response message.
@@ -35,26 +33,37 @@ type Pipe struct {
 
 	Result chan Res
 
-	queue     chan []byte
-	queueSize int
+	queue              chan []byte
+	queueSize          int
+	maxQueueSize       int
+	maxWriteBufferSize int
 
-	m sync.Mutex
+	m *sync.Mutex
+
+	wasExecutedLastTime bool
 }
 
-func NewPipe(host string, port string) (*Pipe, error) {
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
-	if err != nil {
-		return nil, err
+type Config struct {
+	ConnPool           *sync.Pool
+	MaxQueueSize       int
+	MaxWriteBufferSize int
+}
+
+func NewPipe(cfg *Config) *Pipe {
+	pipe := &Pipe{
+		writeBuf:            make([]byte, 4, bufferSize),
+		readBuf:             make([]byte, bufferSize),
+		responseBuf:         make([]byte, 0, bufferSize*2),
+		Result:              make(chan Res),
+		queue:               make(chan []byte),
+		maxQueueSize:        cfg.MaxQueueSize,
+		connPool:            cfg.ConnPool,
+		maxWriteBufferSize:  cfg.MaxWriteBufferSize,
+		m:                   &sync.Mutex{},
+		queueSize:           0,
+		wasExecutedLastTime: false,
 	}
-	return &Pipe{
-		writeBuf:    make([]byte, 0, bufferSize),
-		readBuf:     make([]byte, bufferSize),
-		responseBuf: make([]byte, 0, bufferSize*2),
-		Result:      make(chan Res),
-		queue:       make(chan []byte),
-		m:           sync.Mutex{},
-		conn:        conn,
-	}, nil
+	return pipe
 }
 
 func (pipe *Pipe) Start() {
@@ -62,36 +71,49 @@ func (pipe *Pipe) Start() {
 		select {
 		case data := <-pipe.queue:
 			pipe.m.Lock()
-			l := len(data)
-			if len(pipe.writeBuf)+l > bufferSize || pipe.queueSize == volume {
+			l := len(pipe.writeBuf) + len(data)
+			if l > bufferSize || l > pipe.maxWriteBufferSize {
 				pipe.ExecPipe()
 			}
 
-			if l > 65535 {
-
-			} else {
-				pipe.writeBuf = append(pipe.writeBuf, fastbytes.U16ToB(uint16(l))...)
-			}
 			pipe.writeBuf = append(pipe.writeBuf, data...)
 
 			pipe.queueSize++
-			pipe.m.Unlock()
-		case <-time.After(time.Microsecond * 100):
-			pipe.m.Lock()
-			if pipe.queueSize != 0 {
+			if pipe.queueSize == pipe.maxQueueSize {
 				pipe.ExecPipe()
 			}
+
 			pipe.m.Unlock()
 		}
 	}
 }
 
+func (pipe *Pipe) StartTimer() {
+	for {
+		time.Sleep(100 * time.Microsecond)
+		pipe.m.Lock()
+		if pipe.wasExecutedLastTime {
+			pipe.wasExecutedLastTime = false
+		} else {
+			if pipe.queueSize != 0 {
+				pipe.ExecPipe()
+			}
+		}
+		pipe.m.Unlock()
+	}
+}
+
 func (pipe *Pipe) ExecPipe() {
+	pipe.wasExecutedLastTime = true
 	writeBufLen := len(pipe.writeBuf)
 	if writeBufLen == 0 {
 		return
 	}
-	_, err := pipe.conn.Write(pipe.writeBuf[:writeBufLen])
+	conn := pipe.connPool.Get().(net.Conn)
+	defer pipe.connPool.Put(conn)
+	pipe.writeBuf[0], pipe.writeBuf[1], pipe.writeBuf[2], pipe.writeBuf[3] = byte(writeBufLen), byte(writeBufLen>>8), byte(writeBufLen>>16), byte(writeBufLen>>24)
+	_, err := conn.Write(pipe.writeBuf[:writeBufLen])
+	pipe.writeBuf = pipe.writeBuf[:4]
 	if err != nil {
 		for i := 0; i < pipe.queueSize; i++ {
 			pipe.Result <- Res{
@@ -99,11 +121,11 @@ func (pipe *Pipe) ExecPipe() {
 				Err:   err,
 			}
 		}
+		pipe.queueSize = 0
 		return
 	}
-	pipe.writeBuf = pipe.writeBuf[:0]
 
-	_, err = pipe.conn.Read(pipe.readBuf[:])
+	l, err := conn.Read(pipe.readBuf[:])
 	if err != nil {
 		for i := 0; i < pipe.queueSize; i++ {
 			pipe.Result <- Res{
@@ -111,6 +133,7 @@ func (pipe *Pipe) ExecPipe() {
 				Err:   err,
 			}
 		}
+		pipe.queueSize = 0
 		return
 	}
 
@@ -118,6 +141,21 @@ func (pipe *Pipe) ExecPipe() {
 	for {
 		if pipe.queueSize == 0 {
 			break
+		}
+
+		if offset == l {
+			l, err = conn.Read(pipe.readBuf[:])
+			if err != nil {
+				for i := 0; i < pipe.queueSize; i++ {
+					pipe.Result <- Res{
+						Slice: nil,
+						Err:   err,
+					}
+				}
+				pipe.queueSize = 0
+				return
+			}
+			offset = 0
 		}
 
 		size := int(fastbytes.B2U16(pipe.readBuf[offset : offset+2]))
@@ -131,7 +169,7 @@ func (pipe *Pipe) ExecPipe() {
 			for bufferSize-offset < size {
 				pipe.responseBuf = append(pipe.responseBuf, pipe.readBuf[offset:]...)
 				size -= offset
-				offset, err = pipe.conn.Read(pipe.readBuf[offset:])
+				offset, err = conn.Read(pipe.readBuf[offset:])
 			}
 			pipe.Result <- Res{
 				Slice: pipe.responseBuf[:],
@@ -149,11 +187,62 @@ func (pipe *Pipe) ExecPipe() {
 	}
 }
 
-func (pipe *Pipe) Write(data []byte) {
-	pipe.queue <- data
+func (pipe *Pipe) Ping() chan Res {
+	pipe.queue <- []byte{1, 0, constants.Ping}
+	return pipe.Result
 }
 
-func (pipe *Pipe) Ping() chan Res {
-	pipe.queue <- []byte{constants.Ping}
+func (pipe *Pipe) CreateSpace(engineType constants.SpaceEngineType, size uint16, name []byte) chan Res {
+	length := uint16(len(name)) + 4
+	pipe.queue <- append([]byte{byte(length), byte(length >> 8), constants.CreateSpace, byte(engineType), byte(size), byte(size >> 8)}, name...)
+	return pipe.Result
+}
+
+func (pipe *Pipe) GetSpacesNames() chan Res {
+	pipe.queue <- []byte{1, 0, constants.GetSpacesNames}
+	return pipe.Result
+}
+
+func (pipe *Pipe) Insert(key []byte, value []byte, spaceId uint16) chan Res {
+	keyLength := uint16(len(key))
+	valueLength := uint16(len(value))
+	// spaceId (2) + key length (2)  + msg type(1) = 7
+	length := 5 + keyLength + valueLength
+	slice := make([]byte, 0, length+2)
+	slice = append(slice, byte(length), byte(length>>8), constants.Insert, byte(spaceId), byte(spaceId>>8), byte(keyLength), byte(keyLength>>8))
+	slice = append(slice, key...)
+	slice = append(slice, value...)
+	pipe.queue <- slice
+	return pipe.Result
+}
+
+func (pipe *Pipe) Set(key []byte, value []byte, spaceId uint16) chan Res {
+	keyLength := uint16(len(key))
+	valueLength := uint16(len(value))
+	// spaceId (2) + key length (2)  + msg type(1) = 7
+	length := 5 + keyLength + valueLength
+	slice := make([]byte, 0, length+2)
+	slice = append(slice, byte(length), byte(length>>8), constants.Set, byte(spaceId), byte(spaceId>>8), byte(keyLength), byte(keyLength>>8))
+	slice = append(slice, key...)
+	slice = append(slice, value...)
+	pipe.queue <- slice
+	return pipe.Result
+}
+
+func (pipe *Pipe) Get(key []byte, spaceId uint16) chan Res {
+	length := uint16(len(key)) + 3
+	slice := make([]byte, 0, length+2)
+	slice = append(slice, byte(length), byte(length>>8), constants.Get, byte(spaceId), byte(spaceId>>8))
+	slice = append(slice, key...)
+	pipe.queue <- slice
+	return pipe.Result
+}
+
+func (pipe *Pipe) Delete(key []byte, spaceId uint16) chan Res {
+	length := uint16(len(key)) + 3
+	slice := make([]byte, 0, length+2)
+	slice = append(slice, byte(length), byte(length>>8), constants.Delete, byte(spaceId), byte(spaceId>>8))
+	slice = append(slice, key...)
+	pipe.queue <- slice
 	return pipe.Result
 }
